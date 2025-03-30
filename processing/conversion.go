72821814e9e68e0d28f39af598cc353aefe5af4b
m1_air <peterjbishop.denver@gmail.com>
2025-03-30 16:08:27 -0600

    importer ready to generate datasets

--- processing/conversion.go
+++ processing/conversion.go
@@ -5,14 +5,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func GenerateTripData() {
-	file, err := os.Open("/Users/peterbishop/Development/probable-system/processing/input/trips.txt")
+const outputUrl = "/Users/peterbishop/Development/probable-system/processing/output/"
+const inputUrl = "/Users/peterbishop/Development/probable-system/processing/input/"
+
+func OpenFile(fileName string) ([][]string, error) {
+	file, err := os.Open(inputUrl + fileName)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
-		return
+		return nil, err
 	}
 	defer file.Close()
 
@@ -21,9 +25,19 @@ func GenerateTripData() {
 	records, err := reader.ReadAll()
 	if err != nil {
 		fmt.Println("Error reading CSV:", err)
-		return
+		return nil, err
 	}
 
+	return records, nil
+}
+
+func GenerateTripData() {
+
+	records, err := OpenFile("trips.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
 	var trips []Trip
 	for i, row := range records {
 		if i == 0 {
@@ -51,7 +65,7 @@ func GenerateTripData() {
 		return
 	}
 
-	outputFile := "/Users/peterbishop/Development/probable-system/processing/output/trips.json"
+	outputFile := fmt.Sprintf(outputUrl + "trips.json")
 	err = os.WriteFile(outputFile, jsonData, 0644)
 	if err != nil {
 		fmt.Println("Error writing JSON file:", err)
@@ -59,4 +73,181 @@ func GenerateTripData() {
 	}
 
 	fmt.Println("JSON data successfully saved to", outputFile)
+}
+
+func GenerateRouteData() {
+	records, err := OpenFile("routes.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	var routes []Route
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+
+		routeType := strings.TrimSpace(row[5])
+		routeTypeInt := 0
+		if routeType == "3" {
+			routeTypeInt = 3
+		}
+		routes = append(routes, Route{
+			RouteID:        row[0],
+			AgencyID:       row[1],
+			RouteShortName: row[2],
+			RouteLongName:  row[3],
+			RouteDesc:      row[4],
+			RouteType:      routeTypeInt,
+			RouteURL:       row[6],
+			RouteColor:     row[7],
+			RouteTextColor: row[8],
+		})
+
+	}
+
+	jsonData, err := json.MarshalIndent(routes, "", "  ")
+	if err != nil {
+		fmt.Println("Error converting to JSON:", err)
+		return
+	}
+	outputFile := fmt.Sprintf(outputUrl + "routes.json")
+	err = os.WriteFile(outputFile, jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return
+	}
+	fmt.Println("JSON data successfully saved to", outputFile)
+}
+
+func GenerateShapesData() {
+	records, err := OpenFile("shapes.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	var shapes []Shape
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+
+		shapeDistTraveled := strings.TrimSpace(row[4])
+		shapeDistTraveledFloat := 0.0
+		if shapeDistTraveled != "" {
+			fmt.Sscanf(shapeDistTraveled, "%f", &shapeDistTraveledFloat)
+		}
+		shapePtSequence := strings.TrimSpace(row[3])
+		shapePtSequenceInt := 0
+		if shapePtSequence != "" {
+			fmt.Sscanf(shapePtSequence, "%d", &shapePtSequenceInt)
+		}
+		shapes = append(shapes, Shape{
+			ShapeID: row[0],
+			ShapePtLat: func() float64 {
+				lat, _ := strconv.ParseFloat(row[1], 64)
+				return lat
+			}(),
+			ShapePtLon: func() float64 {
+				lon, _ := strconv.ParseFloat(row[2], 64)
+				return lon
+			}(),
+			ShapePtSequence:   shapePtSequenceInt,
+			ShapeDistTraveled: shapeDistTraveledFloat,
+		})
+
+	}
+
+	jsonData, err := json.MarshalIndent(shapes, "", "  ")
+	if err != nil {
+		fmt.Println("Error converting to JSON:", err)
+		return
+	}
+	outputFile := fmt.Sprint(outputUrl + "shapes.json")
+	err = os.WriteFile(outputFile, jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return
+	}
+	fmt.Println("JSON data successfully saved to", outputFile)
+}
+
+func GenerateStopTimesData() {
+	records, err := OpenFile("stop_times.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	var stopTimes []StopTime
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+		stopSequence, _ := strconv.Atoi(row[4])
+		pickupType, _ := strconv.Atoi(row[6])
+		dropOffType, _ := strconv.Atoi(row[7])
+		stopTimes = append(stopTimes, StopTime{
+			TripID:        row[0],
+			ArrivalTime:   row[1],
+			DepartureTime: row[2],
+			StopID:        row[3],
+			StopSequence:  stopSequence,
+			PickupType:    pickupType,
+			DropOffType:   dropOffType,
+		})
+	}
+
+	jsonData, err := json.MarshalIndent(stopTimes, "", "  ")
+	if err != nil {
+		fmt.Println("Error converting to JSON:", err)
+		return
+	}
+	outputFile := fmt.Sprint(outputUrl + "stop_times.json")
+	err = os.WriteFile(outputFile, jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return
+	}
+	fmt.Println("StopTimes JSON successfully saved to", outputFile)
+}
+
+func GenerateStopsData() {
+	records, err := OpenFile("stops.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	var stops []Stop
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+		lat, _ := strconv.ParseFloat(row[4], 64)
+		lon, _ := strconv.ParseFloat(row[5], 64)
+		stops = append(stops, Stop{
+			StopID:   row[0],
+			StopCode: row[1],
+			StopName: row[2],
+			StopDesc: row[3],
+			StopLat:  lat,
+			StopLon:  lon,
+		})
+	}
+
+	jsonData, err := json.MarshalIndent(stops, "", "  ")
+	if err != nil {
+		fmt.Println("Error converting to JSON:", err)
+		return
+	}
+	outputFile := fmt.Sprint(outputUrl + "stops.json")
+	err = os.WriteFile(outputFile, jsonData, 0644)
+	if err != nil {
+		fmt.Println("Error writing JSON file:", err)
+		return
+	}
+	fmt.Println("Stops JSON successfully saved to", outputFile)
 }
--- processing/datatypes.go
+++ processing/datatypes.go
@@ -9,3 +9,51 @@ type Trip struct {
 	BlockID      string `json:"block_id"`
 	ShapeID      string `json:"shape_id"`
 }
+
+type Route struct {
+	RouteID        string `json:"route_id"`
+	AgencyID       string `json:"agency_id"`
+	RouteShortName string `json:"route_short_name"`
+	RouteLongName  string `json:"route_long_name"`
+	RouteDesc      string `json:"route_desc"`
+	RouteType      int    `json:"route_type"`
+	RouteURL       string `json:"route_url"`
+	RouteColor     string `json:"route_color"`
+	RouteTextColor string `json:"route_text_color"`
+}
+
+type Shape struct {
+	ShapeID           string  `json:"shape_id"`
+	ShapePtLat        float64 `json:"shape_pt_lat"`
+	ShapePtLon        float64 `json:"shape_pt_lon"`
+	ShapePtSequence   int     `json:"shape_pt_sequence"`
+	ShapeDistTraveled float64 `json:"shape_dist_traveled"`
+}
+
+type StopTime struct {
+	TripID            string `json:"trip_id"`
+	ArrivalTime       string `json:"arrival_time"`
+	DepartureTime     string `json:"departure_time"`
+	StopID            string `json:"stop_id"`
+	StopSequence      int    `json:"stop_sequence"`
+	StopHeadsign      string `json:"stop_headsign,omitempty"`
+	PickupType        int    `json:"pickup_type"`
+	DropOffType       int    `json:"drop_off_type"`
+	ShapeDistTraveled string `json:"shape_dist_traveled,omitempty"`
+	Timepoint         int    `json:"timepoint,omitempty"`
+}
+
+type Stop struct {
+	StopID             string  `json:"stop_id"`
+	StopCode           string  `json:"stop_code"`
+	StopName           string  `json:"stop_name"`
+	StopDesc           string  `json:"stop_desc"`
+	StopLat            float64 `json:"stop_lat"`
+	StopLon            float64 `json:"stop_lon"`
+	ZoneID             string  `json:"zone_id,omitempty"`
+	StopURL            string  `json:"stop_url,omitempty"`
+	LocationType       int     `json:"location_type,omitempty"`
+	ParentStation      string  `json:"parent_station,omitempty"`
+	StopTimezone       string  `json:"stop_timezone,omitempty"`
+	WheelchairBoarding int     `json:"wheelchair_boarding,omitempty"`
+}
